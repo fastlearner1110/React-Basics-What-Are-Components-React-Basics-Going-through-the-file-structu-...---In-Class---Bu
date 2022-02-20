f6b97b071015a34d39f2f2f2d911655a95ecd608
fastlearner1110 <95926942+fastlearner1110@users.noreply.github.com>
2022-02-20 21:50:44 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [para,setPara]=useState("");
+  let addPara=()=>{
+    setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <p id="para">{para}</p>
+      <button id="click" onClick={addPara}>click</button>
     </div>
   );
 }
